16cd81ca6cc7683b69975707f08d88cc42d6f097
localadmin <raphael.hiesgen@haw-hamburg.de>
2014-02-24 15:54:17 +0100

    Adds stubs for unspported functions.

--- sys/posix/pthread/pthread_mutex.c
+++ sys/posix/pthread/pthread_mutex.c
@@ -58,3 +58,113 @@ int pthread_mutex_unlock(pthread_mutex_t *mutex)
     return 0;
 }
 
+int pthread_mutex_getprioceiling(const pthread_mutex_t *mutex, int *prioceiling)
+{
+    // TODO
+    (void) mutex;
+    (void) prioceiling;
+    return 0;
+}
+
+int pthread_mutex_setprioceiling(pthread_mutex_t *mutex, int prioceiling, int *old_ceiling)
+{
+    // TODO
+    (void) mutex;
+    (void) prioceiling;
+    (void) old_ceiling;
+    return 0;
+}
+
+int pthread_mutexattr_init(pthread_mutexattr_t *attr)
+{
+    // TODO
+    (void) attr;
+    return 0;
+}
+
+int pthread_mutexattr_destroy(pthread_mutexattr_t *attr)
+{
+    // TODO
+    (void) attr;
+    return 0;
+}
+
+int pthread_mutexattr_getpshared(const pthread_mutexattr_t *attr, int *pshared)
+{
+    // TODO
+    (void) attr;
+    (void) pshared;
+    return 0;
+}
+
+int pthread_mutexattr_setpshared(pthread_mutexattr_t *attr, int pshared)
+{
+    // TODO
+    (void) attr;
+    (void) pshared;
+    return 0;
+}
+
+int pthread_mutexattr_gettype(const pthread_mutexattr_t *attr, int *kind)
+{
+    // TODO
+    (void) attr;
+    (void) kind;
+    return 0;
+}
+
+int pthread_mutexattr_settype(pthread_mutexattr_t *attr, int kind)
+{
+    // TODO
+    (void) attr;
+    (void) kind;
+    return 0;
+}
+
+int pthread_mutexattr_getprotocol(const pthread_mutexattr_t *attr, int *protocol)
+{
+    // TODO
+    (void) attr;
+    (void) protocol;
+    return 0;
+}
+
+int pthread_mutexattr_setprotocol(pthread_mutexattr_t *attr, int protocol)
+{
+    // TODO
+    (void) attr;
+    (void) protocol;
+    return 0;
+}
+
+int pthread_mutexattr_getprioceiling(const pthread_mutexattr_t *attr, int *prioceiling)
+{
+    // TODO
+    (void) attr;
+    (void) prioceiling;
+    return 0;
+}
+
+int pthread_mutexattr_setprioceiling(pthread_mutexattr_t *attr, int prioceiling)
+{
+    // TODO
+    (void) attr;
+    (void) prioceiling;
+    return 0;
+}
+
+int pthread_mutexattr_getrobust(const pthread_mutexattr_t *attr, int *robustness)
+{
+    // TODO
+    (void) attr;
+    (void) robustness;
+    return 0;
+}
+
+int pthread_mutexattr_setrobust(pthread_mutexattr_t *attr, int robustness)
+{
+    // TODO
+    (void) attr;
+    (void) robustness;
+    return 0;
+}
