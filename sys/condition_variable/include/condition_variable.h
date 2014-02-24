9cc6f103a5269d91cc1c1232e6822a1fb6ce5ed1
Martin <Martin.Landsmann@HAW-Hamburg.de>
2014-02-24 14:56:29 +0100

    fix chnged return type description

--- sys/condition_variable/include/condition_variable.h
+++ sys/condition_variable/include/condition_variable.h
@@ -41,21 +41,21 @@ typedef struct {
 
 typedef struct pthread_cond_t {
     /* fields are managed by cv functions, don't touch */
-    unsigned int val;       // @internal
+    unsigned int val;
     queue_node_t queue;     // @internal
 }pthread_cond_t;
 
 /**
  * @brief Initializes a condition attribute variable object using default values
  * @param attr pre-allocated condition attribute variable structure.
- * @return Always returns 0, always succeeds.
+ * @return returns 0 on success, an errorcode otherwise.
  */
 int pthread_cond_condattr_destroy(struct pthread_condattr_t *attr);
 
 /**
  * @brief Uninitializes a condition attribute variable object
  * @param attr pre-allocated condition attribute variable structure.
- * @return Always returns 0, always succeeds.
+ * @return returns 0 on success, an errorcode otherwise.
  */
 int pthread_cond_condattr_init(struct pthread_condattr_t *attr);
 
@@ -63,14 +63,14 @@ int pthread_cond_condattr_init(struct pthread_condattr_t *attr);
  * @brief Initializes a condition variable object
  * @param cond pre-allocated condition variable structure.
  * @param attr pre-allocated condition attribute variable structure.
- * @return Always returns 0, always succeeds.
+ * @return returns 0 on success, an errorcode otherwise.
  */
 int pthread_cond_init(struct pthread_cond_t *cond, struct pthread_condattr_t *attr);
 
 /**
  * @brief Destroy the condition variable cond
  * @param cond pre-allocated condition variable structure.
- * @return Always returns 0, always succeeds.
+ * @return returns 0 on success, an errorcode otherwise.
  */
 int pthread_cond_destroy(struct pthread_cond_t *cond);
 
@@ -78,7 +78,7 @@ int pthread_cond_destroy(struct pthread_cond_t *cond);
  * @brief blocks the calling thread until the specified condition cond is signalled
  * @param cond pre-allocated condition variable structure.
  * @param mutex pre-allocated mutex variable structure.
- * @return Always returns 0, always succeeds.
+ * @return returns 0 on success, an errorcode otherwise.
  */
 int pthread_cond_wait(struct pthread_cond_t *cond, struct mutex_t *mutex);
 
@@ -87,21 +87,21 @@ int pthread_cond_wait(struct pthread_cond_t *cond, struct mutex_t *mutex);
  * @param cond pre-allocated condition variable structure.
  * @param mutex pre-allocated mutex variable structure.
  * @param abstime pre-allocated timeout.
- * @return Always returns 0, always succeeds.
+ * @return returns 0 on success, an errorcode otherwise.
  */
 int pthread_cond_timed_wait(struct pthread_cond_t *cond, struct mutex_t *mutex, const struct timespec *abstime);
 
 /**
  * @brief unblock at least one of the threads that are blocked on the specified condition variable cond
  * @param cond pre-allocated condition variable structure.
- * @return Always returns 0, always succeeds.
+ * @return returns 0 on success, an errorcode otherwise.
  */
 int pthread_cond_signal(struct pthread_cond_t *cond);
 
 /**
  * @brief unblock all threads that are currently blocked on the specified condition variable cond
  * @param cond pre-allocated condition variable structure.
- * @return Always returns 0, always succeeds.
+ * @return returns 0 on success, an errorcode otherwise.
  */
 int pthread_cond_broadcast(struct pthread_cond_t *cond);
 
