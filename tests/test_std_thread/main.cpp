6f3af9a987bb2a4e100c79dbdb216f7a40693b66
localadmin <raphael.hiesgen@haw-hamburg.de>
2014-02-25 10:00:31 +0100

    test for std::thread

---
+++ tests/test_std_thread/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2014 
+ * Copyright (C) 2014 
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @file
+ * @brief       Demonstration of mixed c++ user application with pure c RIOT
+ *
+ * @author      
+ *
+ */
+
+#include <cstdio>
+#include <thread>
+
+void foo()
+{
+    printf("I am f00!");
+}
+
+int main() {
+    printf("Hello c++\n");
+    std::thread bar (foo);
+    bar.join();
+    printf("Done\n");
+}
+
