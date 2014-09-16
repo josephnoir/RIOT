5bee8f2e51c523fd64281836aa14e6ef7f50c4f8
Joseph Noir <joseph.noir@gmail.com>
2014-09-16 17:41:44 +0200

    Add small test for thread, mutex and cond_var

--- examples/caf/main.cpp
+++ examples/caf/main.cpp
@@ -1,23 +1,58 @@
 
+#include <string>
 #include <cstdio>
 
+#include "mutex.hpp"
 #include "thread.hpp"
-//#include "mutex.hpp"
+#include "condition_variable.hpp"
 
 using namespace std;
+using namespace caf;
 
-//void* func(void*) {
-//  printf("Hallo\n");
-//}
-
-/* main */
+/* example from http://en.cppreference.com/w/cpp/thread/condition_variable */
 int main() {
   printf("\n************ RIOT and C++ threads ***********\n");
 
-//  thread t(func);
-  caf::thread t([] {
-    printf("Hello\n");
+  mutex m;
+  condition_variable cv;
+  string data;
+  bool ready = false;
+  bool processed = false;
+ 
+  thread worker([&] {
+    // Wait until main() sends data
+    unique_lock<mutex> lk(m);
+    cv.wait(lk, [&ready]{return ready;});
+ 
+    // after the wait, we own the lock.
+    printf("Worker thread is processing data\n");
+    data += " after processing";
+ 
+    // Send data back to main()
+    processed = true;
+    printf("Worker thread signals data processing completed\n");
+ 
+    // Manual unlocking is done before notifying, to avoid
+    // that the waiting thread gets blocked again.
+    lk.unlock();
+    cv.notify_one();
   });
+ 
+  data = "Example data";
+  // send data to the worker thread
+  {
+    lock_guard<mutex> lk(m);
+    ready = true;
+    printf("main() signals data ready for processing\n");
+  }
+  cv.notify_one();
+
+  // wait for the worker
+  {
+    unique_lock<mutex> lk(m);
+    cv.wait(lk, [&processed]{return processed;});
+  }
+  printf("Back in main(), data = %s\n", data.c_str());;
 
   printf("Bye, bye.\n");
   printf("*********************************************\n");
