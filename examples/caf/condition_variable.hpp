9f3e6cedb1fdc92f3173c194b5f382137e1021dd
Joseph Noir <joseph.noir@gmail.com>
2014-09-16 02:40:32 +0200

    Add thread, mutex and conditon_variable wrapper
    CAF requies these to work, but gcc does not accept the pthread
    implementation to work with its std::thread. These wrappers
    replace the std::threads. However they do only contain the
    necessary functions.

---
+++ examples/caf/condition_variable.hpp
@@ -0,0 +1,99 @@
+
+#ifndef CONDITION_VARIABLE_HPP
+#define CONDITION_VARIABLE_HPP
+
+#include <chrono>
+
+#include "mutex.hpp"
+
+namespace caf {
+
+enum class cv_status { no_timeout, timeout };
+
+class condition_variable {
+  
+ public:
+  using native_handle_type = priority_queue_t*;
+
+  // constexpr condition_variable() : m_queue{NULL} { }
+  inline condition_variable() {
+    m_queue.first = NULL;
+  }
+  ~condition_variable();
+
+  void notify_one() noexcept;
+  void notify_all() noexcept;
+
+  void wait(unique_lock<mutex>& lock) noexcept;
+  template <class Predicate>
+  void wait(unique_lock<mutex>& lock, Predicate pred);
+  template <class Clock, class Duration>
+  cv_status wait_until(unique_lock<mutex>& lock,
+                 const std::chrono::time_point<Clock, Duration>& timeout_time);
+  template <class Clock, class Duration, class Predicate>
+  bool wait_until(unique_lock<mutex>& lock,
+                  const std::chrono::time_point<Clock,Duration>& timeout_time,
+                  Predicate pred);
+  // template <class Rep, class Period>
+  // cv_status wait_for(unique_lock<mutex>& lock,
+  //                    const std::chrono::duration<Rep, Period>& rel_time);
+  // template <class Rep, class Period, class Predicate>
+  // bool wait_for(unique_lock<mutex>& lock,
+  //               const std::chrono::duration<Rep, Period>& rel_time,
+  //               Predicate pred);
+  inline native_handle_type native_handle() { return &m_queue; }
+
+ private:
+  condition_variable(const condition_variable&);
+  condition_variable& operator=(const condition_variable&);
+
+  void do_timed_wait(unique_lock<mutex>& lock,
+                     std::chrono::time_point<std::chrono::system_clock, 
+                                             std::chrono::nanoseconds>) noexcept;
+
+  priority_queue_t m_queue;
+};
+
+template <class T, class Rep, class Period>
+inline typename std::enable_if<std::chrono::__is_duration<T>::value,T>::type
+ceil(std::chrono::duration<Rep, Period> duration) {
+  using namespace std::chrono;
+  T res = duration_cast<T>(duration);
+  if (res < duration) {
+    ++res;
+  }
+  return res;
+}
+
+template <class Predicate>
+void condition_variable::wait(unique_lock<mutex>& lock, Predicate pred) {
+  while(!pred) {
+    wait(lock);
+  }
+}
+
+template <class Clock, class Duration>
+cv_status condition_variable::wait_until(unique_lock<mutex>& lock,
+                                         const std::chrono::time_point<Clock,
+                                                      Duration>& timeout_time) {
+    using namespace std::chrono;
+    wait_for(lock, timeout_time - system_clock::now());
+    return std::chrono::system_clock::now() < timeout_time ?
+      cv_status::no_timeout : cv_status::timeout;
+}
+
+template <class Clock, class Duration, class Predicate>
+bool condition_variable::wait_until(unique_lock<mutex>& lock,
+                    const std::chrono::time_point<Clock,Duration>& timeout_time,
+                    Predicate pred) {
+  while (!pred()) {
+    if (wait_until(lock, timeout_time) == cv_status::timeout) {
+      return pred();
+    }
+  }
+  return true;
+}
+
+} // namespace caf
+
+#endif // CONDITION_VARIABLE_HPP
---
+++ examples/caf/mutex.cpp
@@ -0,0 +1,33 @@
+//
+//  mutex.cpp
+//  
+//
+//  Created by Joseph Noir on 12/09/14.
+//
+//
+
+#include "mutex.hpp"
+
+namespace caf {
+
+// const defer_lock_t defer_lock = {};
+// const try_to_lock_t try_to_lock = {};
+// const adopt_lock_t adopt_lock = {};
+
+mutex::~mutex() {
+  // nop
+}
+
+void mutex::lock() {
+  mutex_lock(&m_mtx);
+}
+
+bool mutex::try_lock() noexcept {
+  return (1 == mutex_trylock(&m_mtx));
+}
+
+void mutex::unlock() noexcept {
+  mutex_unlock(&m_mtx);
+}
+
+} // namespace caf
