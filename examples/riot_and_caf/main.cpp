844d614b466dab479819b700c353df6a7e3ab048
Joseph Noir <joseph.noir@gmail.com>
2014-09-17 15:28:30 +0200

    Add example for CAF on RIOT

--- examples/riot_and_caf/main.cpp
+++ examples/riot_and_caf/main.cpp
@@ -1,126 +1,46 @@
+
 /*
- * Copyright (C) 2014 Hamburg University of Applied Sciences (HAW)
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
+* Copyright (C) 2014 Hamburg University of Applied Sciences (HAW)
+*
+* This file is subject to the terms and conditions of the GNU Lesser
+* General Public License v2.1. See the file LICENSE in the top level
+* directory for more details.
+*/
 
 /**
- * @file        main.cpp
- * @brief       Demonstration of ...
- * @author      Martin Landsmann <martin.landsmann@haw-hamburg.de>
- * @author      Raphael Hiesgen <rapheal.hiesgen@haw-hamburg.de>
- */
+* @file main.cpp
+* @brief Demonstration of ...
+* @author Martin Landsmann <martin.landsmann@haw-hamburg.de>
+* @author Raphael Hiesgen <rapheal.hiesgen@haw-hamburg.de>
+*/
 
 #include <string>
 #include <cstdio>
-
 #include "caf/all.hpp"
+#include "caf/chrono.hpp"
 
 using namespace std;
 using namespace caf;
-/*
-behavior button(event_based_actor* self, const string& identifier) {
-  return {
-    on(atom("press")) >> [=] {
-      printf("%s: You fool! ...\n", identifier.c_str());
-      return atom("quit");
-    },
-    on(atom("quit")) >> [=] {
-      self->quit();
-      //return atom("quit");
-    }
-  };
-}
-
-behavior fool(event_based_actor* self, const string& identifier) {
-  return {
-    on(atom("find"), arg_match) >> [=] (const actor& thing) {
-      printf("%s: Oh a button, lets press it.\n", identifier.c_str());
-      self->send(thing, atom("press"));
-    },
-    on(atom("quit")) >> [=] {
-      self->quit();
-      return atom("quit");
-    }
-  };
-}
-*/
 
 behavior fluffy(event_based_actor* self) {
-  printf("blaaaa\n");
+  printf("It's so fluffy!!1!\n");
   return {
-    on(atom("poke"), arg_match) >> [=](const actor& other) {
-      printf("quiek!\n");
-      self->send(other, atom("blub"));
-      self->quit();
+    on(atom("poke"), arg_match) >> [=](int cnt) {
+      printf("[%06d]\n", cnt);
+      self->delayed_send(self, chrono::microseconds(1000), atom("poke"), cnt + 1);
+    },
+    others() >> [] {
+      printf("Unknown message\n");
     }
   };
 }
-
-/*
-class fluffy : public event_based_actor {
- public:
-  fluffy() {
-    printf("it's fluffy!\n");
-  }
-
-  behavior make_behavior() override {
-    return {
-      on(atom("poke"), arg_match) >> [=](const actor& other) {
-        printf("quiek!\n");
-        send(other, atom("blub"));
-        //return atom("blub");
-      }
-    };
-  }
-};
-*/
-
-/*
-void sender(event_based_actor* self, const actor& other) {
-  self->send(other, atom("blub"));
-}
-*/
 
 int main() {
   printf("\n************ RIOT and CAF demo ***********\n");
-
   auto f = spawn<detached>(fluffy);
-  //auto f = spawn<fluffy,detached>();
-  {
-    scoped_actor self;
-    self->send(f, atom("poke"), self);
-    // self->send(f, atom("quit"));
-    self->receive(
-      others() >> [] {
-        printf("blubb\n");
-      }
-    );
-  }
-
-
-/*
-  auto bttn = spawn<detached>(button, "The Button");
-  auto dude = spawn<detached>(fool, "The Fool");
-  anon_send(dude, atom("find"), bttn);
-*/
-/*
-  {
-    scoped_actor self;
-    //self->send(self, atom("blub"));
-    spawn(sender, self);
-    self->receive(
-      others() >> [] {
-        printf("blubb\n");
-      }
-    );
-  }
-*/
+  anon_send(f, atom("poke"), 0);
   await_all_actors_done();
   shutdown();
-
   printf("******************************************\n");
   return 0;
 }
