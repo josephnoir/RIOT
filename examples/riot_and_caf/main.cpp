802d86858ef8afe25a94f5c0772d7f99555b594a
localadmin <localadmin@mona>
2014-11-06 19:38:17 +0100

    Exit actors

--- examples/riot_and_caf/main.cpp
+++ examples/riot_and_caf/main.cpp
@@ -1,20 +1,21 @@
 /*
-* Copyright (C) 2014 Hamburg University of Applied Sciences (HAW)
-*
-* This file is subject to the terms and conditions of the GNU Lesser
-* General Public License v2.1. See the file LICENSE in the top level
-* directory for more details.
-*/
+ * Copyright (C) 2014 Hamburg University of Applied Sciences (HAW)
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
 
 /**
-* @file main.cpp
-* @brief Demonstration of ...
-* @author Martin Landsmann <martin.landsmann@haw-hamburg.de>
-* @author Raphael Hiesgen <rapheal.hiesgen@haw-hamburg.de>
-*/
+ * @file        main.cpp
+ * @brief       Demonstration of ...
+ * @author      Martin Landsmann <martin.landsmann@haw-hamburg.de>
+ * @author      Raphael Hiesgen <rapheal.hiesgen@haw-hamburg.de>
+ */
 
 #include <string>
 #include <cstdio>
+
 #include "caf/all.hpp"
 #include "caf/chrono.hpp"
 
@@ -34,37 +35,15 @@ behavior fluffy(event_based_actor* self) {
   };
 }
 
-/*
-class fluffy : public event_based_actor {
- public:
-  fluffy() {
-    printf("it's fluffy!\n");
-  }
-
-  behavior make_behavior() override {
-    return {
-      on(atom("poke"), arg_match) >> [=](const actor& other) {
-        printf("quiek!\n");
-        send(other, atom("blub"));
-        //return atom("blub");
-      }
-    };
-  }
-};
-*/
-
-/*
-void sender(event_based_actor* self, const actor& other) {
-  self->send(other, atom("blub"));
-}
-*/
-
 int main() {
   printf("\n************ RIOT and CAF demo ***********\n");
+
   auto f = spawn<detached>(fluffy);
   anon_send(f, atom("poke"), 0);
+
   await_all_actors_done();
   shutdown();
+
   printf("******************************************\n");
   return 0;
 }
