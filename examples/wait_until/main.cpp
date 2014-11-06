d7b9e7d39e3599f31c6168fb35ba8a605db6732a
localadmin <localadmin@mona>
2014-11-06 19:39:59 +0100

    Adjust example

--- examples/wait_until/main.cpp
+++ examples/wait_until/main.cpp
@@ -7,48 +7,45 @@
 
 using namespace std;
 using namespace caf;
- 
+
 condition_variable cv;
 mutex cv_m;
 atomic<int> i = ATOMIC_VAR_INIT(0);
 atomic<int> j = ATOMIC_VAR_INIT(0);
- 
-void waits(int idx)
-{
-    unique_lock<mutex> lk(cv_m);
-    auto now = chrono::system_clock::now();
-    if(cv.wait_until(lk, now + chrono::milliseconds(idx*100), [](){return i == 1;}))
-    {
-        cerr << "Thread " << idx << " finished waiting. i == " << i << '\n';
-    }
-    else
-    {
-        cerr << "Thread " << idx << " timed out. i == " << i << '\n';
-        ++j;
-    }
-}
- 
-void signals()
-{
-    this_thread::sleep_for(chrono::milliseconds(120));
-    cerr << "Notifying...\n";
-    cv.notify_all();
-    this_thread::sleep_for(chrono::milliseconds(100));
-    i = 1;
-    cerr << "Notifying again...\n";
-    cv.notify_all();
-}
- 
-int main()
-{
-    thread t1(waits, 1), t2(waits, 2), t3(waits, 3), t4(signals);
-    /*
-    t1.join(); 
-    t2.join();
-    t3.join();
-    t4.join();
-    */
-    unique_lock<mutex> lk(cv_m);
-    cv.wait(lk, [] { return j==4; });
+
+void waits(int idx) {
+  unique_lock<mutex> lk(cv_m);
+  auto now = chrono::system_clock::now();
+  if(cv.wait_until(lk, now + chrono::milliseconds(idx*100), [](){return i == 1;})) {
+    printf("[waits] Thread %d finished waiting. i == %d\n", idx, i.load());
+  } else {
+    printf("[waits] Thread %d timed out. i == %d\n", idx, i.load());
+  }
+  ++j;
+  cv.notify_all();
 }
 
+void signals() {
+  this_thread::sleep_for(chrono::milliseconds(120));
+  printf("[signals] Making noise...\n");
+  cv.notify_all();
+  this_thread::sleep_for(chrono::milliseconds(100));
+  i = 1;
+  printf("[signals] Making noise again...\n");
+  cv.notify_all();
+}
+
+int main() {
+  thread(waits, 1).detach();
+  thread(waits, 2).detach();
+  thread(waits, 3).detach();
+  thread(signals).detach();
+  printf("[main] waiting for threads\n");
+  unique_lock<mutex> lk(cv_m);
+  auto now = chrono::system_clock::now();
+  if (cv.wait_until(lk, now + chrono::milliseconds(500), [] { return j==3; })) {
+    printf("[main] all threads done\n");
+  } else {
+    printf("[main] let's kill some threads\n");
+  }
+}
