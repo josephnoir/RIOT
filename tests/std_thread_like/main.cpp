cdd3bba4f95b4275ec1912a862a1c5249bed4d60
Joseph Noir <joseph.noir@gmail.com>
2014-11-12 16:04:11 +0100

    Add more tests

--- tests/std_thread_like/main.cpp
+++ tests/std_thread_like/main.cpp
@@ -15,7 +15,7 @@ using namespace caf;
 int main() {
   printf("\n************ RIOT and C++ thread test ***********\n");
  
-  printf("Creating one thread\n");
+  printf("Creating one thread ...\n");
   {
     thread t([] {
       //nop
@@ -28,13 +28,24 @@ int main() {
   }
   printf("Done\n");
 
-  printf("Testing join on 'finished' thread\n");
+  printf("Creating detached thread ...\n");
+  {
+    thread t([] {
+      //nop
+    });
+    assert(t.joinable() == 1);
+    t.detach();
+    assert(t.joinable() == 0);
+  }
+  printf("Done\n");
+
+  printf("Testing join on 'finished' thread ...\n");
   {
     thread t;
     assert(t.joinable() == 0);
 
     t = thread([]{
-      printf("hi\n");
+      // nop
     });
     assert(t.joinable() == 1);
 
@@ -46,33 +57,28 @@ int main() {
   }
   printf("Done\n");
 
-  printf("Testing join on 'running' thread\n");
+  printf("Testing join on 'running' thread ...\n");
   {
     mutex m;
     thread t1,t2;
     condition_variable cv;
     assert(t1.joinable() == 0);
     assert(t2.joinable() == 0);
-    printf("Creating first thread\n");
     t1 = thread([&m,&cv]{
       unique_lock<mutex> lk(m);
       cv.wait(lk);
     });
     assert(t1.joinable() == 1);
-    printf("Creating second thread\n");
     t2 = thread([&t1]{
       try {
-        printf("Joining first thread\n");
         t1.join();
       } catch (const std::system_error& e) {
         assert(false);
       }
     });
     assert(t2.joinable() == 1);
-    printf("Notifying condition\n");
     cv.notify_one();
     try {
-      printf("Joining second thread\n");
       t2.join();
     } catch (const std::system_error& e) {
       assert(false);
